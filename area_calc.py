ce3ff2c8273fa7fab58f5cdb02c1d37a1664c8b0
Endrews <elijah.smiael@gmail.com>
2022-07-28 13:22:45 -0500

    initialize repository

---
+++ area_calc.py
@@ -0,0 +1,14 @@
+# This is a comment
+
+#Basic input and print ---------------------------------
+
+# name = input('Tell me your name, please:')
+# age = input('...and your age:')
+# print(name+',', 'you are', age)
+
+#Calc the area of a circle -----------------------------
+
+# radius = input('Enter the radius of your circle (ft):')
+# area = 3.142*int(radius)**2
+# print('The area of your circle is', area)
+
